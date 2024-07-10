a93e2669d1b221f0065bbca8be1cf18dca274447
thomasborde94 <thomas.borde94@hotmail.fr>
2024-07-10 15:55:16 +0200

    Displaying platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Flex, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, Flex, Image, Text, Box } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
@@ -10,20 +10,24 @@ const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={5} overflow="hidden">
       <CardBody backgroundColor="#171d50">
-        <Flex alignItems="center">
+        <Flex direction="column" alignItems="flex-start">
           <Image
             src={game.background_image}
             boxSize={{ base: "50px", sm: "100px", md: "150px", lg: "200px" }}
             borderRadius="50%"
             objectFit="cover"
           />
-          <Text
-            fontSize={{ base: "md", sm: "lg", md: "xl", lg: "2xl" }}
-            color="white"
-            ml={4}
-          >
-            {game.name}
-          </Text>
+          <Box mt={2}>
+            <Text
+              fontSize={{ base: "md", sm: "lg", md: "xl", lg: "2xl" }}
+              color="white"
+            >
+              {game.name}
+            </Text>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+          </Box>
         </Flex>
       </CardBody>
     </Card>
---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,47 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { Text, Stack, Icon, HStack } from "@chakra-ui/react";
+import { IconType } from "react-icons/lib";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  // maps each platform to its icon
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    android: FaAndroid,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+  return (
+    <HStack
+      spacing={2}
+      fontSize={{ base: "md", sm: "lg", md: "xl", lg: "2xl" }}
+    >
+      {platforms.map((platform) => (
+        <Text key={platform.id} color="white">
+          <Icon as={iconMap[platform.slug]} color="gray.200" />
+        </Text>
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
