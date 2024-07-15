7de27731d0718b218b4ff62e9c75a44125723cc6
thomasborde94 <thomas.borde94@hotmail.fr>
2024-07-15 11:19:16 +0200

    handled games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,8 @@
-// crops the images so that they are not HD and take too long to load
+import noImage from '../assets/Placeholder_view_vector.svg.png'
 
+// crops the images so that they are not HD and take too long to load
 const getCroppedImageUrl = (url:string) => {
-    if (!url) return ''
+    if (!url) return noImage
     const target = 'media/'
     const index = url.indexOf(target) + target.length
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index)
