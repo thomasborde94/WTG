495e8bf0c3d3b8f41d371362db8b3745805ec577
thomasborde94 <thomas.borde94@hotmail.fr>
2024-07-23 10:41:47 +0200

    fixed typo

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,12 +2,13 @@ import jwt from 'jsonwebtoken'
 
 export default (req, res, next) => {
     try {
-        const token = req.headers.authoziration.split(' ')[1];
+        const token = req.headers.authorization.split(" ")[1];
         const decodedToken = jwt.verify(token, 'RADOM_TOKEN_SECRET');
         const userId = decodedToken.userId;
         req.auth = {
             userId: userId
         }
+        next()
     } catch(error) {
         res.status(401).json({error})
     }
